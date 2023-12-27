a56b777c0d3f9d57b04e0086f35204c3f083d21e
rameshbommidi <rameshbommidi125@gmail.com>
2023-12-27 14:16:10 +0000

    done

---
+++ src/components/TodoItem/index.js
@@ -0,0 +1,35 @@
+// Write your code here
+
+import {Component} from 'react'
+
+import './index.css'
+
+class TodoItem extends Component {
+  render() {
+    const {eachObject, key, onDelete} = this.props
+
+    const {id, title} = eachObject
+
+    this.deleteFunction = () => {
+      onDelete(id)
+    }
+
+    return (
+      <li key={key} className="line">
+        <div>
+          <p className="para">{title}</p>
+        </div>
+        <div>
+          <button
+            onClick={this.deleteFunction}
+            className="button"
+            type="button"
+          >
+            Delete
+          </button>
+        </div>
+      </li>
+    )
+  }
+}
+export default TodoItem
